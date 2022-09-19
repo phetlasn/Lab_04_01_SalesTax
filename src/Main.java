a11f38da6b861a020787a289236944b3286fd88a
phetlasn <Souriya.n.phet@gmail.com>
2022-09-18 23:24:26 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 22.50;
+        double salesTax;
+        salesTax = price * 0.05;
+
+        System.out.println("The tax applied to the product will be $ " + salesTax);
+    }
+}
